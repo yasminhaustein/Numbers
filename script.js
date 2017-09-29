994401d8a158d3eb2bae792b6c745f1b4f07188c
yasminhaustein <yasmin.haustein@mailbox.tu-dresden.de>
2017-09-29 16:13:04 +0200

    trying to let the number vanish after a moment

--- script.js
+++ script.js
@@ -2,13 +2,30 @@
  * Created by yasminhaustein on 29.09.17.
  */
 document.onload = start();
+var div;
 var randomNumber;
+var number_of_Digits;
+var start;
+var div = document.getElementById("number");
+
 function start() {
+    number_of_Digits = 3;
     generateNumber();
 }
 
 function generateNumber() {
-    randomNumber = Math.random();
-    alert(randomNumber);
-    document.getElementById("number").innerHTML = randomNumber;
+    div = document.getElementById("number");
+    start = Date.now();
+    randomNumber = Math.floor(Math.random() * Math.pow(10, number_of_Digits));
+
+    div.textContent = randomNumber;
+    number_of_Digits++;
+}
+
+function vanishNumber() {
+    var end = Date.now();
+    while (end < start + 10000) {
+        end = Date.now();
+    }
+    div.textContent = "";
 }
