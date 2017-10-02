630f84d18ad9a4462c4327f081eeb275b16816c5
yasminhaustein <yasmin.haustein@mailbox.tu-dresden.de>
2017-10-02 16:10:32 +0200

    implemented game logic

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 /**
  * Created by yasminhaustein on 29.09.17.
  */
-document.onload = start();
-var randomNumber;
+window.onload = start;
+var randomNumber = [];
 var currentInput = [];
 var number_of_Digits;
 var div = document.getElementById("number");
+var numberpad = document.getElementById("numberpad");
 
 function start() {
     number_of_Digits = 3;
@@ -13,29 +14,56 @@ function start() {
 }
 
 function generateNumber() {
+    numberpad.style.display = "none";
+    randomNumber = [];
+    currentInput = [];
     div = document.getElementById("number");
-    start = Date.now();
-    randomNumber = Math.floor(Math.random() * Math.pow(10, number_of_Digits));
-
-    div.textContent = randomNumber;
+    for (var i = 0; i < number_of_Digits; i++) {
+        randomNumber.push(Math.floor(Math.random() * 10));
+    }
+    div.textContent = stringOf(randomNumber);
     number_of_Digits++;
-    window.setTimeout(vanishNumber,3000);
+    window.setTimeout(vanishNumber, 3000);
 }
 
 function vanishNumber() {
     div.textContent = "";
+    numberpad.style.display = "block";
+
 }
 
 function numberInput(n) {
     currentInput.push(n);
-    div.textContent = currentInput.toString();
+    div.textContent = stringOf(currentInput);
 }
 
 function eraseNumber() {
     currentInput.pop();
-    div.textContent = currentInput.toString();
+    div.textContent = stringOf(currentInput);
 }
 
 function checkNumber() {
-    generateNumber();
+    var check;
+    if (randomNumber.length == currentInput.length) {
+        for (var i = 0; i < randomNumber.length; i++) {
+            if (randomNumber[i] != currentInput[i]) {
+                check = true;
+            }
+        }
+        if (!check) generateNumber();
+        else gameOver();
+    }
+    else gameOver();
+}
+
+function stringOf(x) {
+    var string = [];
+    for (var i = 0; i < x.length; i++) {
+        string = string + x[i];
+    }
+    return string;
+}
+
+function gameOver() {
+    alert("Game Over!");
 }
